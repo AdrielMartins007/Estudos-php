03bb5aa04f85b167c6eb0ae996180a20c96c4c0c
Adriel Windows <adrielcostamartins608@gmail.com>
2026-02-15 19:30:14 -0300

    testando novas variaveis

--- curso-em-video/variaveis-tipos/variaveis.php
+++ curso-em-video/variaveis-tipos/variaveis.php
@@ -28,7 +28,17 @@
 
     const PAIS = 'Brasil'; /* DECLARANDO UMA CONSTANTE, NAO É OBRIGATORIO SER MAISCULA, MAS POR PADROES DE MERCADO É BOM COLOCAR, TAMBEM PARA DIFERENCIAR */
 
-    print 'meu nome é ' . $nome . ' e moro no ' . PAIS . ' e tenho ' . $idade . ' anos <br>'; /* EXIBIÇÃO DAS VARIAVEIS E USANDO O '.' PARA COCA*/
+    print 'Meu nome é ' . $nome . ' e moro no ' . PAIS . ' e tenho ' . $idade . ' anos.<br>'; /* EXIBIÇÃO DAS VARIAVEIS E USANDO O '.' PARA COCATENAR*/
+
+    echo "Meu nome é $nome e moro no " . PAIS . " e tenho $idade anos e o ano atual é " . date('Y') . ".\n"; /* OUTRA FORMA DE CACATENAR É DENTRO DAS ASPAS, MAS PRA ISSO É NECESSÁRIO ASPAS DUPLAS, POREM, ISSO NAO FUNCIONA COM CONSTANTES PQ ELE NAO CONSEGUE INTERPRETAR, ENTAO, É NECESSARIO COLOCAR PONTOS PARA COCATENAR.*/
+
+    /* É IMPORTANTE COLOCAR ASPAS DUPLAS POIS É NECESSARIO INTERPRETAR AS INFORMAÇÕES QUE ESTAO DENTRO DAS VARIAVEIS, AO USAR ASPAS SIMPLES, VC TA DIZENDO QUE NAO PRECISA INTERPRETAR NADA, SO MOSTRAR AQUILO QUE ESTÁ DENTRO DAS ASPAS, E OBVIAMENTE ELE VAI MOSTRAR OQ ESTÁ ESCRITO '$nome' ou '$idade'. */
+
+    echo <<< FRASE
+
+        Olá, estou testando a forma de escrever uma frase completa dentro do echo
+        me chamo $nome e tenho $idade, estou amando aprender programação.
+    FRASE; /* ESSA É A FORMA DE ESCREVER UMA FRASE GRANDE DENTRO DO PHP EM MULTIPLAS LINHAS, ONDE VC PODE PEGAR AS VARIAVEIS*/
 
     var_dump($idade); /* COMANDO PARA MOSTRAR O TIPO PRIMITIVO DA VARIAVEL */
     
