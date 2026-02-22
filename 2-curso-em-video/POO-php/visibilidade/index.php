36fc844cb5b7da31e20e93077f0bf5b2507eee41
Adriel Windows <adrielcostamartins608@gmail.com>
2026-02-21 21:13:24 -0300

    upload

--- 2-curso-em-video/POO-php/visibilidade/index.php
+++ 2-curso-em-video/POO-php/visibilidade/index.php
@@ -5,13 +5,8 @@
     $car1 = new Carros;
     $car1->novo = true;
     $car1->cor = "Branco";
-    $car1->chave = true; /* PROTEGIDO */
-    $car1->motor = true; /* PROTEGIDO */
-    $car1->modelo = "Fiat-uno"; /* PRIVADO */
 
-    $car1->ligar(); /* PRIVADO */
     $car1->mensagem();
-    $car1->vender(); /* PRIVADO */
     $car1->revisao();
     $car1->tipo();
 
