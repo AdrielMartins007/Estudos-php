d797a8a1170749793a1108edcd401cdba364616e
Adriel Windows <adrielcostamartins608@gmail.com>
2026-02-25 23:34:05 -0300

    projeto finalizado

--- 3-desafios-pessoais/2-POO/6-jogo-luta/Jogo.php
+++ 3-desafios-pessoais/2-POO/6-jogo-luta/Jogo.php
@@ -1,64 +1,70 @@
 <?php
 
-class JogoLuta{
+class Lutadores
+{
+    public $nome;
+    public $vida;
+    public $ataque;
 
-    public $lut1; /* lutador numero 1 */
-    public $lut2; /* lutador numero 2 */
-    public $lut1Vida;
-    public $lut2Vida;
-    public $lut1Forca;
-    public $lut2Forca;
-
-    public function __construct($lut1, $lut2)
+    public function __construct($nome)
     {
-        $this->lut1 = $lut1;
-        $this->lut2 = $lut2;
-        $this->lut1Vida = 100;
-        $this->lut2Vida = 100;
-        $this->lut1Forca = 100;
-        $this->lut2Forca = 100;
+        $this->nome = $nome;
+        $this->vida = 100;
+        $this->ataque = "";
     }
 
-    public function iniciar(){
-        echo "TODOS PRONTOS? LUTEM...<br><br>";
+    public function facada($adversario)
+    {
+        $adversario->vida -= 30;
+
+        echo $this->nome . " da uma facada...<br><br>";
     }
 
-    public function atqBolso(){
+    public function tiro($adversario)
+    {
+        $adversario->vida -= 45;
 
-        echo $this->lut1 . " ATACOU...<br><br>";
-
-        $this->lut2Vida += 10;
-        $this->lut1Vida -= 10;
-        
-        echo $this->lut1 . ": Tome seu cachaceiro<br>";
-        echo $this->lut2 . ": Calma companheiro<br><br>";
-
-        echo "Status:<br>";
-        echo "lutador: " . $this->lut1 . "<br>Vida: " . $this->lut2Vida . "<br>";
-        echo "Lutador: " . $this->lut2 . "<br>Vida: " . $this->lut1Vida . "<br><br>";
+        echo $this->nome . " da um tiro certeiro...<br><br>";
     }
 
-    public function atqLula(){
-        echo $this->lut2 . " ATACOU...<br><br>";
+    public function habilidEspecial($adversario)
+    {
+        $adversario->vida -= 60;
 
-        $this->lut1Vida += 10;
-        $this->lut2Vida -= 10;
-        
-        echo $this->lut2 . ": Toma bolsominion<br>";
-        echo $this->lut1 . ": Ai, seu filho da put#<br><br>";
+        echo $this->nome . " ativa sua habilidade especial...<br><br>";
+    }
 
-        echo "Status:<br>";
-        echo "lutador: " . $this->lut1 . "<br>Vida: " . $this->lut2Vida . "<br>";
-        echo "Lutador: " . $this->lut2 . "<br>Vida: " . $this->lut1Vida;
+    public function status(){
+        echo "Status: <br>";
+        echo "Lutador: " . $this->nome . "<br>";
+        echo "Vida: " . $this->vida . "<br>";
     }
 }
-?>
 
-<?php
+class Jogo
+{
+    public $lutador1;
+    public $lutador2;
 
-$user1 = new JogoLuta("BOLSONARO", "LULA");
-$user1->iniciar();
-$user1->atqBolso();
-$user1->atqLula();
+    public function __construct($l1, $l2)
+    {
+        $this->lutador1 = $l1;
+        $this->lutador2 = $l2;
+    }
 
-?>
+    public function iniciar()
+    {
+        echo "O Jogo ja vai começar...<br>";
+        echo "Preparado " . $this->lutador1->nome . "?";
+        echo " Preparado " . $this->lutador2->nome . "?<br>";
+        echo "Vamos começar....<br><br>";
+    }
+
+    public function status(){
+        $this->lutador1->status();
+        $this->lutador2->status();
+    }
+}
+
+
+
