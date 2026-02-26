bd1c2d548ad06ccf041754eb304747e16a97ef01
Adriel Martins <adrielcostamartins608@gmail.com>
2026-02-26 11:43:20 -0300

    informando os valores dos metodos

--- 2-curso-em-video/POO-php/4-encapsulamento/Controle-remoto.php
+++ 2-curso-em-video/POO-php/4-encapsulamento/Controle-remoto.php
@@ -12,43 +12,63 @@ class ControleRemoto implements Controlador{ /* Criação da classe controle rem
         $this->tocando = false;
     }
 
-    function volume(){ /* Criação da funcao de volume */
-
+    function volume($volume){ /* Criação da funcao de volume */
+        $this->volume = $volume;
     }
 
     function getVolume(){ /* criação da funcao para retornar o valor de volume */
         return $this->volume;
+
+        if($this->volume == true){
+            echo "Nao mudo";
+        } else {
+            echo "Mudo";
+        }
     }
 
-    function ligado(){
-
+    function ligado($ligado){
+        $this->ligado = $ligado;
     }
 
     function getLigado(){
         return $this->ligado;
+
+        if($this->ligado == true){
+            echo "Ligado";
+        } else {
+            echo "Desligado";
+        }
     }
 
-    function tocando(){
-        
+    function tocando($tocando){
+        $this->tocando = $tocando;
     }
 
     function getTocando(){
         return $this->tocando;
+
+        if($this->tocando == true){
+            echo "Está tocando";
+        } else {
+            echo "Nao tá tocando";
+        }
     }
 
 
     /* Implementando as funcoes da interface para dentro da classe controle remoto */
 
     public function ligar(){
-
+        $this->ligado(true);
     }
 
     public function desligar(){
-
+        $this->ligado(false);
     }
 
     public function abrirMenu(){
-
+        echo "Ligado: " . $this->getLigado();
+        echo "Volume: " . $this->getVolume();
+        echo "Tocando: " . $this->getTocando();
     }
 
     public function fecharMenu(){
@@ -56,27 +76,27 @@ class ControleRemoto implements Controlador{ /* Criação da classe controle rem
     }
 
     public function maisVolume(){
-
+        $this->volume($this->volume += 5);
     }
 
     public function menosVolume(){
-
+        $this->volume($this->volume -= 5);
     }
 
     public function ligarMudo(){
-
+        $this->volume(false);
     }
 
     public function desligarMudo(){
-
+        $this->volume(true);
     }
 
     public function play(){
-
+        $this->tocando(true);
     }
 
     public function pause(){
-
+        $this->tocando(false);
     }
 }
 
