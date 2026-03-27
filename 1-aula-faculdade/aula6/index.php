3e395b31226e7ce7950ddcd08eb52095cf87a167
Adriel Windows <adrielcostamartins608@gmail.com>
2026-03-27 00:37:47 -0300

    anotações sobre comando diversos em strings

--- 1-aula-faculdade/aula6/index.php
+++ 1-aula-faculdade/aula6/index.php
@@ -1,5 +1,17 @@
 <?php 
 
+echo pi() . "<br>";
+/* valor pi */
 
+echo (min(0, 150, 30, 20, -8, -200)) . "<br>"; /* valor minimo */
+echo (max(0, 150, 30, 20, -8, -200)) . "<br>";/* valor maximo */
+
+echo(abs(-6.7)) . "<br>"; /* valor absoluto */
+
+echo(sqrt(64)) . "<br>"; /* raiz quadrada */
+
+echo round(0.49)  . "<br>"; /* para arredondar o valor */
+
+echo rand(0, 100) . "<br>"; /* para gerar numeros aleatorios */
 
 ?>
